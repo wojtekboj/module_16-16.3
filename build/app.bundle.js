34b3adc088e9e46209d60e49fe243df50850f12d
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-07-18 09:09:49 +0200

    Modification app.bundle.js

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -16,19 +16,19 @@
             get: n
         })
     }, r.r = function (e) {
-        "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
-            value: "Module"
-        }), Object.defineProperty(e, "__esModule", {
+        'undefined' != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+            value: 'Module'
+        }), Object.defineProperty(e, '__esModule', {
             value: !0
         })
     }, r.t = function (e, t) {
         if (1 & t && (e = r(e)), 8 & t) return e;
-        if (4 & t && "object" == typeof e && e && e.__esModule) return e;
+        if (4 & t && 'object' == typeof e && e && e.__esModule) return e;
         var n = Object.create(null);
-        if (r.r(n), Object.defineProperty(n, "default", {
+        if (r.r(n), Object.defineProperty(n, 'default', {
                 enumerable: !0,
                 value: e
-            }), 2 & t && "string" != typeof e)
+            }), 2 & t && 'string' != typeof e)
             for (var o in e) r.d(n, o, function (t) {
                 return e[t]
             }.bind(null, o));
@@ -44,7 +44,7 @@
         return Object.prototype.hasOwnProperty.call(e, t)
     }, r.p = "", r(r.s = 0)
 }([function (e, t, r) {
-    "use strict";
+    'use strict';
     r.r(t);
-    console.log("hello world!")
+    console.log('hello world!')
 }]);
